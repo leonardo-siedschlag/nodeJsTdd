dd8696b6d7fd389c9ede5f7a33b35c1947c5e330
Leonardo Siedschlag <siedschlag75@gmail.com>
2021-03-18 17:39:18 -0300

    Revert "test: teste"
    This reverts commit 894462da2cf935672d0d0951e9d7b36763605ef2.

--- src/presentation/controlers/signup.spec.ts
+++ src/presentation/controlers/signup.spec.ts
@@ -1,5 +1,5 @@
 import { SignUpController } from './signup'
-//teste
+
 describe('SignUp Controller', () => {
     test('Should return 400 if no name is provided', () => {
         const sut  = new SignUpController()
